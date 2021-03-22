8f85c857d356bcb012618ce42a1e0b5a7aa7ef02
Sky <RolyPolie@users.noreply.github.com>
2021-03-22 13:21:30 -0700

    Updated Nav Styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Nav() {
     return (
-        <div className="container max-w-screen-2xl text-white absolute">
+        <div className="container max-w-screen-2xl absolute">
             <div className=""> {/*TODO: add sticky nav*/}
                 <nav className="flex justify-center">
-                    <ul className="flex flex-row">
-                        <li className="pr-5 m-5">Home</li>
-                        <li className="pr-5 m-5">Projects</li>
-                        <li className="pr-5 m-5">Contact</li>
+                    <ul className="flex text-4xl tracking-widest">
+                        <li className={"m-10 p-3 bg-gradient-to-r from-green-400 to-blue-500 rounded-2xl shadow-2xl"}>HOME</li>
+                        <li className={"m-10 p-3 bg-gradient-to-r from-green-400 to-blue-500 rounded-2xl shadow-2xl"}>PROJECTS</li>
+                        <li className={"m-10 p-3 bg-gradient-to-r from-green-400 to-blue-500 rounded-2xl shadow-2xl"}>CONTACT</li>
                     </ul>
                 </nav>
             </div>
