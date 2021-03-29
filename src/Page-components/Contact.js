29710234ce76d24e8cd923ea162b7001b7abc4b9
Sky <RolyPolie@users.noreply.github.com>
2021-03-29 13:48:03 -0700

    removed "Formik" and updated form code

--- src/Page-components/Contact.js
+++ src/Page-components/Contact.js
@@ -3,6 +3,12 @@ import React from "react"
 function Contact() {
     return (
         <div className="w-screen h-screen bg-gray-300">
+            <div className="container mx-auto h-screen py-4 grid grid-cols-2 gap-4">
+                <p className="flex justify-center items-center">THIS IS GRID 1</p>
+                <div className="flex justify-center items-center">
+                </div>
+
+            </div>
 
         </div>
     )
