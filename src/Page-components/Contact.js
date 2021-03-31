84373435ad585099744539df7791cc1a045b3888
Sky <RolyPolie@users.noreply.github.com>
2021-03-31 08:24:15 -0700

    added Contacts page + content

--- src/Page-components/Contact.js
+++ src/Page-components/Contact.js
@@ -1,12 +1,109 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Contact() {
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+
+    // const handleChange = event => {
+    //     setFirstName(event.target.first)
+    //     setLastName(event.target.last)
+    // }
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        alert('A name was submitted: ' + firstName + " " + lastName);
+    }
     return (
         <div className="w-screen h-screen bg-gray-300">
-            <div className="container mx-auto h-screen py-4 grid grid-cols-2 gap-4">
-                <p className="flex justify-center items-center">THIS IS GRID 1</p>
-                <div className="flex justify-center items-center">
+            <div className="container mx-auto h-screen py-4 grid grid-cols-2">
+                <div className="grid grid-cols-1">
+                    <h1 className="flex justify-center items-center font-majorMonoDisplay" style={{fontSize: 70}}>
+                        CONTACT ME
+                    </h1>
+                    <p className="text-center text-2xl font-montserrat">
+                    Thanks for taking an interest in my portfolio, if you have any questions
+                    or Inquiries , please feel free to ask using the form.
+                    </p>
                 </div>
+
+
+                <div className="grid grid-cols-1">
+                    <div className="flex justify-center items-center">
+                        <form onSubmit={handleSubmit}>
+                            <div className="flex">
+                                <div className="flex flex-col pr-6">
+                                    <label>
+                                        First Name
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        value={firstName} 
+                                        onChange={(e) => setFirstName(e.target.value)} 
+                                        name="first"
+                                    />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>
+                                        Last Name
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        value={lastName} 
+                                        onChange={(e) => setLastName(e.target.value)} 
+                                        name="last"
+                                    />
+                                </div>
+                            </div>
+                            
+                            <div>
+                                <div className="flex flex-col pt-6">
+                                    <label>
+                                        Email
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        value={lastName} 
+                                        onChange={(e) => setLastName(e.target.value)} 
+                                        name="last"
+                                    />
+                                </div>
+                                <div className="flex flex-col pt-6">
+                                    <label>
+                                        Subject
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        value={lastName} 
+                                        onChange={(e) => setLastName(e.target.value)} 
+                                        name="last"
+                                    />
+                                </div>
+                                <div className="flex flex-col pt-6">
+                                    <label>
+                                        Message
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        value={lastName} 
+                                        onChange={(e) => setLastName(e.target.value)} 
+                                        name="last"
+                                    />
+                                </div>
+                            </div>
+
+                                
+                            
+                            <button 
+                            type="submit" 
+                            value="Submit"
+                            >
+                            Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                
 
             </div>
 
