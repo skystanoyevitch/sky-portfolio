18637b0c8db05f60c637545f3a4eff7df910a23f
Sky <RolyPolie@users.noreply.github.com>
2022-03-24 09:31:38 -0700

    fixed nav not scrolling to top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Nav from "./Page-components/Nav";
 import MainContent from "./Page-components/Main";
+import ScrollToTop from "./Page-components/Helper-components/ScrollToTop";
 import {
 	BrowserRouter as Router,
 	Route,
@@ -13,12 +14,12 @@ import Showcase from "./Page-components/Showcase";
 import Contact from "./Page-components/Contact";
 // import Footer from "./Page-components/Footer";
 
-
 const App = ({ location }) => (
 	<Router>
 		<>
 			{/* {location.pathname !== "/Projects" && <Nav />} */}
-			<Nav style={{ color: '#114B5F' }} />
+			<Nav style={{ color: "#114B5F" }} />
+			<ScrollToTop />
 			<Switch>
 				<Route exact path="/">
 					<MainContent />
