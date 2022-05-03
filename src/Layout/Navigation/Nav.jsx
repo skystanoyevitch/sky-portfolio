64d5db10c9e7b07b5f9f97e9a1736884aa7f0ab3
Sky <RolyPolie@users.noreply.github.com>
2022-05-03 13:40:58 -0700

    removed unused navlink

--- src/Layout/Navigation/Nav.jsx
+++ src/Layout/Navigation/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion, useCycle } from "framer-motion";
 
 const menuVar = {
