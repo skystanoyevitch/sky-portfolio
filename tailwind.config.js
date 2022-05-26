95b7e4e55b24bbe4db1b56dd2072a808949edc6d
Sky <RolyPolie@users.noreply.github.com>
2022-05-26 10:01:43 -0700

    updated main profile photo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 				caveat: ['"Caveat"', "cursive"],
 			},
 			backgroundImage: (theme) => ({
-				profileImage: "url('./images/skyavatar.svg')",
+				profileImage: "url('./images/mainpicsky.jpg')",
 				travelpal: "url('./images/travelpal.png')",
 				teknipak: "url('./images/teknipak.png')",
 				skycafe: "url('./images/skyscoffee.png')",
@@ -39,7 +39,7 @@ module.exports = {
 			boxShadow: {
 				mainBoxShadow:
 					"2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02),6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028),12.5px 12.5px 10px rgba(0, 0, 0, 0.035),22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042),41.8px 41.8px 33.4px rgba(0, 0, 0, 0.05),100px 100px 80px rgba(0, 0, 0, 0.07)",
-				shadowOrange: "7px 7px 0 rgba(255, 87, 34, 1)",
+				shadowGray: "5px 5px 0 rgba(55, 65, 81, 1)",
 			},
 		},
 		divideColor: (theme) => ({
