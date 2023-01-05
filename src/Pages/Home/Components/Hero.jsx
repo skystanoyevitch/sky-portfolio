fe4ac98dc470e1ef803940c6a977a7588e05205f
Sky <sky.stanoyevitch@gmail.com>
2023-01-05 10:50:01 -0800

    refactored hero banner

---
+++ src/Pages/Home/Components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+// import toast, { Toaster } from "react-hot-toast";
+// const notify = () => toast("Oppsies, Coming Soon i promise!");
+
+// const buttonVarant = {
+// 	hover: {
+// 		scale: 1.03,
+// 	},
+// };
+function Hero() {
+	return (
+		<div className="w-screen h-screen">
+			<div className="lg:h-5/6">
+				<div className="container mx-auto flex h-3/4">
+					<div className="flex flex-col flex-1 justify-center">
+						<h1 className="text-8xl">Web Designer | Developer</h1>
+						<p className="lg:pt-8 lg:pb-4 lg:text-xl">
+							helping small businesses grow through digital
+							solutions
+						</p>
+						<button className="rounded-lg bg-[#e75d48] lg:w-1/6 lg:p-2 text-white">
+							Contact
+						</button>
+					</div>
+					<div className="flex-1 bg-heroImg bg-no-repeat"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Hero;
