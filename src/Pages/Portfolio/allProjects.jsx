5540228078eede909b5f3d5ecc278f254e72271a
Sky <sky.stanoyevitch@gmail.com>
2023-01-05 07:32:40 -0800

    minor update

--- src/Pages/Portfolio/allProjects.jsx
+++ src/Pages/Portfolio/allProjects.jsx
@@ -1,20 +1,28 @@
+// import axios from "axios";
+
+
+
 export const allProjects = [
 	{
 		name: "Travel Pal",
 		id: 0,
 		to: "https://traveling-pal.netlify.app/",
 		thumbnail: "bg-travelpal bg-cover bg-center bg-no-repeat rounded-lg",
+		language: 'Nothing'
 	},
 	{
 		name: "Coffee Shop",
 		id: 2,
 		to: "https://rolypolie.github.io/SkysCafe/",
 		thumbnail: "bg-skycafe bg-cover bg-center bg-no-repeat rounded-lg",
+		language: ""
 	},
 	{
 		name: "Teknipak",
 		id: 3,
 		to: "https://teknipak.com/",
 		thumbnail: "bg-teknipak bg-cover bg-center bg-no-repeat rounded-lg",
+		language: 'nothing'
 	},
 ];
+
