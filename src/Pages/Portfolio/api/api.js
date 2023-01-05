92fe8bc3e980493291ff0bee480b93227e6a3702
Sky <sky.stanoyevitch@gmail.com>
2023-01-05 07:33:28 -0800

    removed old api component + added new API comp

---
+++ src/Pages/Portfolio/api/api.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+
+// const cafeShopLanguages = "https://api.github.com/repos/skystanoyevitch/Cafe-Shop/languages"
+
+// export async function getGitHubRepoStats(setCafeLanguages) {
+//         try {
+//             const res = await axios.get(cafeShopLanguages)
+//             setCafeLanguages(res.data)
+//             Object.keys(res.data).map((item => item))
+//         } catch (error) {
+//             console.log(error)
+//         }
+// }
