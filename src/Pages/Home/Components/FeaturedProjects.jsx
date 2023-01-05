753780bfdee915b4bb134b3a52d1d0e198fb3f08
Sky <sky.stanoyevitch@gmail.com>
2023-01-05 13:11:29 -0800

    refactor

--- src/Pages/Home/Components/FeaturedProjects.jsx
+++ src/Pages/Home/Components/FeaturedProjects.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import { useInView } from "react-intersection-observer";
 import Arrows from "../../../icons/Arrows";
 
+import WebsiteIcon from "../../../images/website.svg";
+import CreativeIcon from "../../../images/creative-team.svg";
+import WordPressIcon from "../../../images/wordpress-icon.svg";
+
 const aboutSectionVariant = {
 	initial: {
 		opacity: 0,
@@ -25,33 +29,52 @@ function FeaturedProjects() {
 			variants={aboutSectionVariant}
 			initial="initial"
 			animate={inView ? "animate" : "initial"}
-			className="container mx-auto"
+			className="container mx-auto h-screen"
 		>
-			<div className="flex justify-between">
-				<div className="border-2 border-orange-500 lg:h-80 lg:w-80 bg-gray-200 rounded-lg lg:p-4">
-					<h1 className="text-center">Web Design</h1>
-					<p>
-						Stand out from the growded market with a design
-						taiolored to your business, custom built using the
-						latest UI and UX design principles that results in an
-						engaging website that you and your visitors will love.
-					</p>
+			<div className="flex justify-evenly">
+				<div className="lg:w-80 border-2 border-orange-500 rounded-lg lg:p-4 shadow-mainBoxShadow">
+					<img src={CreativeIcon} alt="" srcset="" />
+
+					<div className="lg:p-6">
+						<h1 className="text-center lg:text-3xl lg:pb-4 font-poppins font-bold text-gray-800">
+							Web Design
+						</h1>
+						<p className="text-md font-poppins font-lite text-center">
+							Stand out from the growded market with a design
+							taiolored to your business, custom built using the
+							latest UI and UX design principles that results in
+							an engaging website that you and your visitors will
+							love.
+						</p>
+					</div>
 				</div>
-				<div className="border-2 border-orange-500 lg:h-80 lg:w-80 bg-gray-200 rounded-lg lg:p-4">
-					<h1 className="text-center">Web Development</h1>
-					<p>
-						Enjoy a Custom Built website using the latest
-						technoligies with an emphasis on performance and
-						security to get your business up and running on the web.
-					</p>
+				<div className="lg:w-80 border-2 border-orange-500 rounded-lg lg:p-4 shadow-mainBoxShadow">
+					<img src={WebsiteIcon} alt="" srcset="" />
+
+					<div className="lg:p-6">
+						<h1 className="text-center lg:text-3xl lg:pb-4 font-poppins font-bold text-gray-800">
+							Web Development
+						</h1>
+						<p className="text-md font-poppins font-lite text-center">
+							Enjoy a Custom Built website using the latest
+							technoligies with an emphasis on performance and
+							security to get your business up and running on the
+							web.
+						</p>
+					</div>
 				</div>
-				<div className="border-2 border-orange-500 lg:h-80 lg:w-80 bg-gray-200 rounded-lg lg:p-4">
-					<h1 className="text-center">WordPress</h1>
-					<p>
-						From custom designs that are mobile responsive to
-						maintanence and repairs to get your site back like new
-						again, wory free.
-					</p>
+				<div className="lg:w-80 border-2 border-orange-500 rounded-lg lg:p-4 shadow-mainBoxShadow">
+					<img src={WordPressIcon} alt="" srcset="" />
+					<div className="lg:p-6">
+						<h1 className="text-center lg:text-3xl lg:pb-4 font-poppins font-bold text-gray-800">
+							WordPress
+						</h1>
+						<p className="text-md font-poppins font-lite text-center">
+							From custom designs that are mobile responsive to
+							maintanence and repairs to get your site back like
+							new again, wory free.
+						</p>
+					</div>
 				</div>
 			</div>
 		</section>
