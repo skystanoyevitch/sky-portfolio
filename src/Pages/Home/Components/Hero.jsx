a7a6d3bdf815f00010a39e7b196c0639b722eb95
Sky <sky.stanoyevitch@gmail.com>
2023-01-06 11:16:47 -0800

    fixed sizing on mobile

--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -4,11 +4,11 @@ import React from "react";
 
 function Hero() {
 	return (
-		<div className="w-screen h-[60vh] lg:h-[80vh]">
+		<div className="w-screen h-[70vh] lg:h-[80vh]">
 			<div className="lg:h-4/6">
 				<div className="container mx-auto flex p-8 lg:p-0 lg:h-full">
 					<div className="flex flex-col flex-1 justify-center">
-						<h1 className="text-5xl lg:text-8xl font-poppins text-gray-800">
+						<h1 className="text-6xl lg:text-8xl font-poppins text-gray-800">
 							Web Designer | Developer
 						</h1>
 						<p className="text-md py-4 lg:py-8 lg:text-xl font-poppins text-gray-600">
