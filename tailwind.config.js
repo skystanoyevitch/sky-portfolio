e6e16ab98b0f282c87eb89e639eb8baa7a70cb72
Sky <sky.stanoyevitch@gmail.com>
2023-01-10 14:48:04 -0800

    added custom box-shadow config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,7 @@ module.exports = {
 				mainBoxShadow:
 					"2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02),6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028),12.5px 12.5px 10px rgba(0, 0, 0, 0.035),22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042),41.8px 41.8px 33.4px rgba(0, 0, 0, 0.05),100px 100px 80px rgba(0, 0, 0, 0.07)",
 				shadowGray: "5px 5px 0 rgba(55, 65, 81, 1)",
+				shadowNeo: "9px 9px 18px #c5c5c5, -9px -9px 18px #fbfbfb",
 			},
 		},
 		divideColor: (theme) => ({
