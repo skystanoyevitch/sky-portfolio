0da33b5cb9f109575d4a607efd44083d09dd23cb
skysLaptop <sky.stanoyevitch@gmail.com>
2023-03-22 10:41:42 -0700

    added words to dictionary settings

--- src/Pages/Home/Homepage.js
+++ src/Pages/Home/Homepage.js
@@ -2,7 +2,7 @@ import Hero from "./Components/Hero";
 import FeaturedProjects from "./Components/FeaturedProjects";
 import OurWork from "./Components/OurWork";
 import ContactComponent from "./Components/ContactComponent";
-import Portfolio from "./Components/Portfolio";
+// import Portfolio from "./Components/Portfolio";
 
 const Homepage = () => {
   return (
