539b8361a07180c2bd9701544ef3ccdd0780db4c
skysLaptop <sky.stanoyevitch@gmail.com>
2023-03-22 10:40:27 -0700

    removed portfolio section

--- src/Pages/Home/Homepage.js
+++ src/Pages/Home/Homepage.js
@@ -10,7 +10,7 @@ const Homepage = () => {
       <Hero />
       <FeaturedProjects />
       <OurWork />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <ContactComponent />
     </div>
   );
