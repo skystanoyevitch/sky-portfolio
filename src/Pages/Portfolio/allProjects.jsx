0690348bef29ebbae5d20fd5da859029ebd45b12
skysLaptop <sky.stanoyevitch@gmail.com>
2023-06-12 10:35:20 -0700

    fixes

--- src/Pages/Portfolio/allProjects.jsx
+++ src/Pages/Portfolio/allProjects.jsx
@@ -1,18 +1,18 @@
 // import axios from "axios";
 
 export const allProjects = [
-	{
-		name: "Part Time Professionals",
-		id: 2,
-		to: "#",
-		thumbnail: "bg-ptpImg bg-cover bg-center bg-no-repeat",
-		language: "",
-	},
-	{
-		name: "Teknipak",
-		id: 3,
-		to: "https://teknipak.com/",
-		thumbnail: "bg-teknipak bg-cover bg-center bg-no-repeat",
-		language: "",
-	},
+  {
+    name: "Part Time Professionals",
+    id: 2,
+    to: "#",
+    thumbnail: "bg-ptpImg bg-cover bg-center bg-no-repeat",
+    language: "",
+  },
+  {
+    name: "Teknipak",
+    id: 3,
+    to: "https://teknipak.com/",
+    thumbnail: "bg-teknipak bg-cover bg-center bg-no-repeat",
+    language: "",
+  },
 ];
