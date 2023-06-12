fec7f47cdf417245ab7abb706db5ab3429cdf996
skysLaptop <sky.stanoyevitch@gmail.com>
2023-06-12 10:32:56 -0700

    removed and added correct components

--- src/Pages/Home/Homepage.js
+++ src/Pages/Home/Homepage.js
@@ -1,16 +1,16 @@
 import Hero from "./Components/Hero";
-import FeaturedProjects from "./Components/FeaturedProjects";
+// import FeaturedProjects from "./Components/FeaturedProjects";
 import OurWork from "./Components/OurWork";
 import ContactComponent from "./Components/ContactComponent";
+import Portfolio from "./Components/Portfolio";
 // import Portfolio from "./Components/Portfolio";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
-      <FeaturedProjects />
       <OurWork />
-      {/* <Portfolio /> */}
+      <Portfolio />
       <ContactComponent />
     </div>
   );
