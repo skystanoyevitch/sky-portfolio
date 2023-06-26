71b223572e0cdba98416cb6a6f2546ea5b0bf1b2
skysLaptop <sky.stanoyevitch@gmail.com>
2023-06-26 09:21:55 -0700

    removed componnent

--- src/Pages/Home/Homepage.js
+++ src/Pages/Home/Homepage.js
@@ -1,6 +1,6 @@
 import Hero from "./Components/Hero";
 // import FeaturedProjects from "./Components/FeaturedProjects";
-import OurWork from "./Components/OurWork";
+// import OurWork from "./Components/OurWork";
 import ContactComponent from "./Components/ContactComponent";
 import Portfolio from "./Components/Portfolio";
 import AboutMe from "./Components/AboutMe";
