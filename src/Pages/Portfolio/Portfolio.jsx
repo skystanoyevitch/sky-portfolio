9b1fdb8218efeb914b987231940ffc26407d5237
skysLaptop <sky.stanoyevitch@gmail.com>
2023-08-11 15:06:18 -0700

    fixed according layout

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -6,19 +6,17 @@ function Portfolio() {
   return (
     <>
       <div className="h-screen text-white flex flex-col justify-center">
-        <div className="container mx-auto">
+        <div className="h-1/2 container mx-auto w-80 md:w-full md:max-w-3xl">
           <div className="">
-            <div className="border">
-              <button
-                type="button"
-                className={`border h-20 ${
-                  buttonOpen ? "h-60" : "h-20"
-                } w-full transition-all`}
-                onClick={() => setButtonOpen(!buttonOpen)}
-              >
-                Col 1
-              </button>
-            </div>
+            <button
+              type="button"
+              className={`border h-20 ${
+                buttonOpen ? "h-60" : "h-20"
+              } transition-all w-full`}
+              onClick={() => setButtonOpen(!buttonOpen)}
+            >
+              <div className="text-left pl-8">PROJECT ONE</div>
+            </button>
           </div>
         </div>
       </div>
