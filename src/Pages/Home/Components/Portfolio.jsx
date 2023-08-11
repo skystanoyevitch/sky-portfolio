741afe54d7393a8dc493c96e64cd37eacc80e25f
skysLaptop <sky.stanoyevitch@gmail.com>
2023-08-11 12:57:52 -0700

    changed project card style

--- src/Pages/Home/Components/Portfolio.jsx
+++ src/Pages/Home/Components/Portfolio.jsx
@@ -45,10 +45,10 @@ function Portfolio() {
               className="container mx-auto px-8 lg:px-0 space-y-10"
             >
               <li className="h-40 w-60 md:w-full md:h-52 lg:h-80">
-                <div className="h-full w-full p-1 bg-gradient-to-r from-cyan-500 via-indigo-500 to-[#e75d48]">
+                <div className="h-full w-full p-1">
                   <div className="h-full w-full relative">
                     <div
-                      className={`${project.thumbnail} absolute h-full w-full md:my-0`}
+                      className={`${project.thumbnail} rounded-md absolute h-full w-full md:my-0 border-b-4 border-orange-700`}
                     ></div>
                   </div>
                 </div>
