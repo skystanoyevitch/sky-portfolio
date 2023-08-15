fded0fbb58de6e22ccde53af8595652179743df1
Sky <sky.stanoyevitch@gmail.com>
2023-08-15 12:52:04 -0700

    all: rounded corners of project cards

--- src/Pages/Home/Components/Portfolio.jsx
+++ src/Pages/Home/Components/Portfolio.jsx
@@ -28,7 +28,7 @@ function Portfolio() {
 			className="container mx-auto px-8 py-32 lg:py-0 lg:h-screen lg: flex flex-col lg:flex-col lg:place-content-center"
 		>
 			<div className="mb-6 lg:mb-16 text-center container mx-auto max-w-lg">
-				<h1 className="text-2xl lg:text-4xl font-poppins font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 via-indigo-500 to-[#e75d48]">
+				<h1 className="text-2xl md:text-4xl font-poppins font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 via-indigo-500 to-[#e75d48]">
 					Featured Projects
 				</h1>
 				<p className="text-md md:text-xl font-light pt-3 lg:text-2xl tracking-wide text-[#9CCDE9]">
@@ -37,7 +37,7 @@ function Portfolio() {
 				</p>
 			</div>
 
-			<div className=" rounded-md md:container mx-auto lg:max-w-4xl md:space-y-0 flex flex-col space-y-16 md:flex md:flex-row md:space-x-6 lg:space-x-16 p-8 lg:p-10 bg-gray-800">
+			<div className=" rounded-md md:container mx-auto lg:max-w-4xl md:space-y-0 flex flex-col space-y-16 md:flex md:flex-row md:space-x-6 lg:space-x-16 p-8 lg:p-14 bg-gray-800">
 				{allProjects.map((project, key) => {
 					return (
 						<ul
@@ -46,9 +46,9 @@ function Portfolio() {
 						>
 							<li className="h-40 w-60 md:w-full md:h-52 lg:h-80">
 								<div className="h-full w-full">
-									<div className="h-full w-full p-2 md:p-4 outline outline-[#9CCDE9] outline-1 hover:outline-orange-700 hover:outline-4 transition-all ease-in-out">
+									<div className="rounded-lg h-full w-full p-2 md:p-4 outline outline-[#9CCDE9] outline-2 md:outline-4 hover:outline-orange-700 hover:outline-4 transition-all ease-in-out">
 										<div
-											className={`${project.thumbnail} h-full w-full md:my-0`}
+											className={`${project.thumbnail} h-full w-full md:my-0 rounded-lg`}
 										></div>
 									</div>
 								</div>
