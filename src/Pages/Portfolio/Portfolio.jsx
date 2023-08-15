6c27d9495042e2a5e9b2043ba7d835066b10ed57
Sky <sky.stanoyevitch@gmail.com>
2023-08-15 16:23:46 -0700

    added logic to toggle button state for projects

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
+import { allProjects } from "./allProjects";
 
 function Portfolio() {
-	const [buttonOpen, setButtonOpen] = useState(false);
-	console.log(buttonOpen);
+	const [buttonOpen, setButtonOpen] = useState({});
+
+	const handleClick = (id) => {
+		setButtonOpen((prev) => ({
+			...prev,
+			[id]: !prev[id],
+		}));
+	};
+
 	return (
 		<>
 			<div className="h-screen text-white flex flex-col justify-center">
-				<div className="h-1/2 container mx-auto w-80 md:w-full md:max-w-3xl">
-					<div
-						className={`${
-							buttonOpen ? "h-60" : "h-20"
-						} rounded-lg transition-all w-80 md:w-full md:max-w-3xl border border-[#9CCDE9] border-b-8 border-r-8 shadow-md`}
-					>
-						<button
-							type="button"
-							className="h-20 w-full"
-							onClick={() => setButtonOpen(!buttonOpen)}
-						>
-							<div className="text-left pl-8 font-sriracha">PROJECT ONE</div>
-						</button>
-					</div>
+				<div className="h-full pt-24 md:pt-0 md:h-1/2 container mx-auto w-80 md:w-full md:max-w-2xl lg:max-w-3xl space-y-12">
+					{allProjects.map((project) => {
+						return (
+							<ul className="">
+								<li
+									key={project.id}
+									className={`${
+										buttonOpen[project.id]
+											? "h-120"
+											: "h-40"
+									} rounded-lg transition-all z-0 w-80 md:w-full md:max-w-3xl border border-[#9CCDE9] border-b-8 border-r-8 shadow-md`}
+								>
+									<button
+										type="button"
+										className="h-20 w-full z-100"
+										onClick={() => handleClick(project.id)}
+									>
+										<div className="text-left pl-8 font-sriracha">
+											{project.name}
+										</div>
+									</button>
+								</li>
+							</ul>
+						);
+					})}
 				</div>
 			</div>
 		</>
--- src/Pages/Portfolio/allProjects.jsx
+++ src/Pages/Portfolio/allProjects.jsx
@@ -1,18 +1,20 @@
 // import axios from "axios";
 
 export const allProjects = [
-  {
-    name: "theparttimeprofesionals.com",
-    id: 2,
-    to: "#",
-    thumbnail: "bg-ptpImg bg-cover bg-center bg-no-repeat",
-    language: "",
-  },
-  {
-    name: "Teknipak",
-    id: 3,
-    to: "https://teknipak.com/",
-    thumbnail: "bg-jobBoardBg bg-cover bg-center bg-no-repeat",
-    language: "",
-  },
+	{
+		name: "theparttimeprofesionals.com",
+		id: 2,
+		to: "#",
+		thumbnail: "bg-ptpImg bg-cover bg-center bg-no-repeat",
+		language: "",
+		active: false,
+	},
+	{
+		name: "Teknipak",
+		id: 3,
+		to: "https://teknipak.com/",
+		thumbnail: "bg-jobBoardBg bg-cover bg-center bg-no-repeat",
+		language: "",
+		active: false,
+	},
 ];
