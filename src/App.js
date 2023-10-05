01aeb2d0c449cafb4541eb2d44f7219492df2131
skysLaptop <sky.stanoyevitch@gmail.com>
2023-10-05 15:57:31 -0700

    removed navlinks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import { Nav } from "./Layout/Navigation/Nav";
 import Homepage from "./Pages/Home/Homepage";
 import ScrollToTop from "./Global-Components/ScrollToTop";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Page Component Imports //
-// import Portfolio from "./Pages/Home/Components/Portfolio";
 import Footer from "./Layout/Navigation/Footer";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 
-const navLinks = [
-  { id: 0, name: "Home", to: "/" },
-  { id: 1, name: "Portfolio", to: "/portfolio" },
-];
+// const navLinks = [
+//   { id: 0, name: "Home", to: "/" },
+//   { id: 1, name: "Portfolio", to: "/portfolio" },
+// ];
 const App = () => (
   <BrowserRouter>
     <div className="bg-[#1A1D25]">
