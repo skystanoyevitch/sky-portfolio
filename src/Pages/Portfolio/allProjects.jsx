f4c4f09043beb84fd62ca280650aa2a3d10d2a1b
skysLaptop <sky.stanoyevitch@gmail.com>
2023-10-04 18:36:13 -0700

    Merge branch 'master' of https://github.com/skystanoyevitch/react-portfolio

--- src/Pages/Portfolio/allProjects.jsx
+++ src/Pages/Portfolio/allProjects.jsx
@@ -1,18 +1,26 @@
 // import axios from "axios";
 
 export const allProjects = [
-  {
-    name: "theparttimeprofesionals.com",
-    id: 2,
-    to: "#",
-    thumbnail: "bg-ptpImg bg-cover bg-center bg-no-repeat",
-    language: "",
-  },
-  {
-    name: "Teknipak",
-    id: 3,
-    to: "https://teknipak.com/",
-    thumbnail: "bg-jobBoardBg bg-cover bg-center bg-no-repeat",
-    language: "",
-  },
+	{
+		name: "Robot Shop App",
+		id: 2,
+		to: "",
+		thumbnail: "bg-robotShop bg-cover bg-center bg-no-repeat",
+		language: "",
+		active: false,
+		link: "https://github.com/skystanoyevitch/joes-robot-shop",
+		description:
+			"An Web app where you can shop for individual Robot parts and add them to your cart. Built using Angular to showcase functionality using a class based approach with dependacny injection, data binding and Templates. ",
+	},
+	{
+		name: "Developer Job Board",
+		id: 3,
+		to: "https://job-board-app-pink.vercel.app/",
+		thumbnail: "bg-techJobs bg-cover bg-no-repeat bg-center",
+		language: "",
+		active: false,
+		link: "https://job-board-app-pink.vercel.app/",
+		description:
+			"Job Board web app Built from scractch with ability to add a custom job post while also allowing the user to search for specific jobs posts. Dev Stack includes react as the frontend, Tailwind/DaisyUI for Styling, NodeJS for the backend and MongoDB for the database.",
+	},
 ];
