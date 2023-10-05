45500626f46ed3f97ecfba0fa2e8e1d08a65a0b5
skysLaptop <sky.stanoyevitch@gmail.com>
2023-10-04 23:44:27 -0700

    removed homepage content + added btns to title

--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import { VscGithub, VscTwitter } from "react-icons/vsc";
 
+//third party imports
+import { FaXTwitter } from "react-icons/fa6";
+
+
 function Hero() {
   const navigate = useNavigate();
 
@@ -13,47 +17,28 @@ function Hero() {
     <div className="w-screen h-120 flex flex-col justify-center md:h-screen">
       <div className="lg:h-full">
         <div className="container mx-auto flex flex-col lg:justify-center lg:flex-row p-8 lg:p-8 xl:p-0 lg:h-full">
-          <div className="flex flex-col justify-center container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl">
+          <div className="flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl">
             <div className="">
               <p className="text-4xl md:text-5xl xl:text-8xl font-sriracha text-neutral-300">
                 Heyo!
               </p>
-              <div className="text-4xl md:text-5xl xl:text-8xl font-sriracha h-16 md:h-36 text-neutral-300">
+              <div className="text-4xl md:text-5xl xl:text-8xl font-sriracha text-neutral-300">
                 i'm{" "}
                 <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 via-indigo-500 to-[#e75d48]">
                   Sky
                 </span>
                 , Software engineer and Designer
               </div>
-              {/* <p className="text-md md:text-xl font-light my-3 lg:text-2xl tracking-wide text-[#9CCDE9]">
-                Frontend Developer{" "}
-                <span className="text-[#e75d48] font-normal"> | </span> Designer
-              </p> */}
             </div>
-
-            {/* <div className="flex justify-center">
-              <a href="#section-contact" onClick={(e) => e.preventDefault}>
-                <button className="rounded-lg flex py-1 px-4 md:py-1 lg:px-6 lg:py-1 text-md md:text-base lg:text-lg bg-gradient-to-r from-cyan-500 via-indigo-500 to-[#e75d48] transition-all border-2 border-[#1A1D25] md:hover:border-[#e75d48] md:hover:bg-none md:hover:border-b-4 text-white focus:scale focus:scale-90 md:hover:scale-100">
-                  Contact
-                </button>
+            <div className="text-white font-mono flex space-x-4">
+              <button type="button" className="border rounded-xl py-1 px-2">
+                Projects
+              </button>
+              <a href="dfasdf" type="button" className="border rounded-xl py-1 px-2 flex place-items-center space-x-2">
+                <FaXTwitter />
+                <div>Contact Me</div>
               </a>
             </div>
-            <div className="flex justify-center space-x-4 text-3xl xl:text-4xl mt-6">
-              <a href="https://github.com/skystanoyevitch" target="blank">
-                <VscGithub
-                  style={{ color: "#9CCDE9" }}
-                  onMouseOver={({ target }) => (target.style.color = "#348ec2")}
-                  onMouseOut={({ target }) => (target.style.color = "#9CCDE9")}
-                />
-              </a>
-              <a href="https://twitter.com/Sky_webdev" target="blank">
-                <VscTwitter
-                  style={{ color: "#9CCDE9" }}
-                  onMouseOver={({ target }) => (target.style.color = "#348ec2")}
-                  onMouseOut={({ target }) => (target.style.color = "#9CCDE9")}
-                />
-              </a>
-            </div> */}
           </div>
         </div>
       </div>
--- src/Pages/Home/Homepage.js
+++ src/Pages/Home/Homepage.js
@@ -1,15 +1,14 @@
 import Hero from "./Components/Hero";
 import ContactComponent from "./Components/ContactComponent";
-import Portfolio from "./Components/Portfolio";
-import AboutMe from "./Components/AboutMe";
+// import Portfolio from "./Components/Portfolio";
+// import AboutMe from "./Components/AboutMe";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
-      <AboutMe />
-      {/* <OurWork /> */}
-      <Portfolio />
+      {/* <AboutMe />
+      <Portfolio /> */}
       <ContactComponent />
     </div>
   );
