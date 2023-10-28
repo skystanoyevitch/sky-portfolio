cf62f8107f3946b305595df5b0986ba72ab311fb
Sky <sky.stanoyevitch@gmail.com>
2023-10-28 14:22:33 -0700

    deleted legacy code/components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import Homepage from "./Pages/Home/Homepage";
-import ScrollToTop from "./Global-Components/ScrollToTop";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Page Component Imports //
 import Footer from "./Layout/Navigation/Footer";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 
-// const navLinks = [
-//   { id: 0, name: "Home", to: "/" },
-//   { id: 1, name: "Portfolio", to: "/portfolio" },
-// ];
+
 const App = () => (
   <BrowserRouter>
     <div className="bg-[#060116]">
-      {/* <Nav navLinks={navLinks} /> */}
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/portfolio" element={<Portfolio />} />
--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -20,18 +20,13 @@ function Hero() {
 				.timeline()
 				.fromTo(
 					".heyo",
-					{ y: "-1000" },
-					{ y: "0", duration: 0.8, ease: "bounce.out" }
+					{ opacity: 0, ease: 0.2, duration: 1 },
+					{ opacity: 1, ease: 0.2, duration: 1 }
 				)
 				.fromTo(
 					".box",
 					{ opacity: 0, ease: 0.2, duration: 1 },
 					{ opacity: 1, ease: 0.2, duration: 1 }
-				)
-				.fromTo(
-					".btns",
-					{ opacity: 0, ease: 0.2 },
-					{ opacity: 1, ease: 0.2 }
 				);
 		}, comp);
 
@@ -46,32 +41,33 @@ function Hero() {
 				<div className="container mx-auto flex flex-col lg:justify-center lg:flex-row p-8 lg:p-8 xl:p-0 lg:h-full">
 					<div
 						ref={comp}
-						className="flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl"
+						className=" flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl"
 					>
 						<p className="heyo text-5xl md:text-6xl xl:text-8xl font-caveat font-bold text-neutral-300">
 							Heyo{" "}
 							{/* <span className="text-3xl md:text-7xl">🤙</span> */}
 						</p>
-						<div className="box text-5xl md:text-5xl xl:text-8xl font-poppins font-bold text-neutral-300">
-							I'm Sky, a Software Engineer and Designer.{" "}
-							{/* <span className="md:text-4xl lg:text-7xl">🧑</span> */}
-						</div>
-						<div className="btns text-white text-sm md:text-base font-poppins font-light flex space-x-4 pt-4">
-							<a
-								href="/portfolio"
-								className="bg-orange-600 rounded-lg py-1 px-3 flex place-items-center"
-							>
-								{/* <FaLaptop /> */}
-								<div>My Projects</div>
-							</a>
-							<a
-								href="https://twitter.com/Sky_webdev"
-								target="blank_"
-								className="bg-cyan-600 rounded-lg py-1 px-3 flex place-items-center"
-							>
-								{/* <FaXTwitter /> */}
-								<div>Contact Me</div>
-							</a>
+
+						<div className="box">
+							<div className="text-5xl md:text-5xl xl:text-8xl font-poppins font-bold text-neutral-300">
+								I'm Sky, a Software Engineer and Designer.{" "}
+								{/* <span className="md:text-4xl lg:text-7xl">🧑</span> */}
+							</div>
+							<div className="text-white text-sm md:text-base font-poppins font-light flex space-x-4 pt-8">
+								<a
+									href="/portfolio"
+									className="bg-orange-600 rounded-lg py-1 px-3 flex place-items-center"
+								>
+									<div>My Projects</div>
+								</a>
+								<a
+									href="https://twitter.com/Sky_webdev"
+									target="blank_"
+									className="bg-cyan-600 rounded-lg py-1 px-3 flex place-items-center"
+								>
+									<div>Contact Me</div>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
