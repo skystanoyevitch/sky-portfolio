eff847dc1643326ab81736d64e0c5493f537d860
Sky <sky.stanoyevitch@gmail.com>
2023-10-29 12:43:25 -0700

    changed title font and styles

--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -43,27 +43,29 @@ function Hero() {
 						ref={comp}
 						className=" flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl"
 					>
-						<p className="heyo text-5xl md:text-6xl xl:text-8xl font-caveat font-bold text-neutral-300">
+						{/* <p className="heyo text-5xl md:text-6xl xl:text-8xl font-poppins font-light text-neutral-300">
 							Heyo{" "}
-							{/* <span className="text-3xl md:text-7xl">🤙</span> */}
-						</p>
+						</p> */}
 
 						<div className="box">
-							<div className="text-5xl md:text-5xl xl:text-8xl font-poppins font-bold text-neutral-300">
-								I'm Sky, a Software Engineer and Designer.{" "}
-								{/* <span className="md:text-4xl lg:text-7xl">🧑</span> */}
+							<div className="text-5xl md:text-5xl xl:text-8xl font-caveat text-neutral-300">
+								Heyo i'm Sky,
+								<br />{" "}
+								<span className="font-mono">
+									[Software Engineer and Designer]{" "}
+								</span>
 							</div>
-							<div className="text-white text-sm md:text-base font-poppins font-light flex space-x-4 pt-8">
+							<div className="text-white text-sm md:text-base font-poppins font-semibold flex space-x-4 pt-8">
 								<a
 									href="/portfolio"
-									className="bg-orange-600 rounded-lg py-1 px-3 flex place-items-center"
+									className="bg-none border-2 border-orange-500 rounded-lg py-1 px-3 flex place-items-center"
 								>
 									<div>My Projects</div>
 								</a>
 								<a
 									href="https://twitter.com/Sky_webdev"
 									target="blank_"
-									className="bg-cyan-600 rounded-lg py-1 px-3 flex place-items-center"
+									className="bg-none border-2 border-cyan-500 rounded-lg py-1 px-3 flex place-items-center"
 								>
 									<div>Contact Me</div>
 								</a>
