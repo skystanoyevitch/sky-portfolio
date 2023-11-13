e31669f67eb943abec742d8119358201c5d1d588
Sky <sky.stanoyevitch@gmail.com>
2023-11-12 16:16:31 -0800

    removed project from list

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -7,16 +7,18 @@ function Portfolio() {
 		<>
 			<div className="h-screen text-white flex flex-col justify-center">
 				<div className="h-full py-24 md:pt-0 md:h-1/2 container mx-auto w-80 md:w-full md:max-w-2xl lg:max-w-3xl space-y-8 md:space-y-10">
-					<a href="/" className="text-5xl text-red-400">
-						<BsArrowLeftCircle />
-					</a>
+					<div className="text-5xl text-red-400 transition-all hover:scale-105 hover:text-red-300">
+						<a href="http:/">
+							<BsArrowLeftCircle />
+						</a>
+					</div>
 
 					{allProjects.map((project) => {
 						return (
 							<ul className="">
 								<li
 									key={project.id}
-									className={` transition-all w-80 z-0 md:w-full md:max-w-3xl hover:scale-105 hover:underline hover:underline-offset-8`}
+									className={` transition-transform w-80 z-0 md:w-full md:max-w-3xl hover:scale-105 hover:underline hover:underline-offset-8 hover:decoration-red-400 hover:decoration-4`}
 								>
 									<a
 										href={project.link}
--- src/Pages/Portfolio/allProjects.jsx
+++ src/Pages/Portfolio/allProjects.jsx
@@ -2,16 +2,6 @@
 
 export const allProjects = [
 	{
-		name: "Robot Shop",
-		id: 2,
-		to: "",
-		thumbnail: "",
-		language: "",
-		active: false,
-		link: "https://github.com/skystanoyevitch/joes-robot-shop",
-		description: "",
-	},
-	{
 		name: "Developer Job Board",
 		id: 3,
 		to: "https://job-board-app-pink.vercel.app/",
