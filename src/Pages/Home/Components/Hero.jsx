298224804036061d355200667e4bf3360f118ab1
skysLaptop <sky.stanoyevitch@gmail.com>
2023-11-26 14:09:00 -0800

    changed gradients to all colors

--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -6,74 +6,74 @@ import { useNavigate } from "react-router-dom";
 import { gsap } from "gsap";
 
 function Hero() {
-	const navigate = useNavigate();
-	const comp = useRef();
-	const tl = useRef();
+  const navigate = useNavigate();
+  const comp = useRef();
+  const tl = useRef();
 
-	useEffect(() => {
-		navigate("/", { replace: true });
-	}, [navigate]);
+  useEffect(() => {
+    navigate("/", { replace: true });
+  }, [navigate]);
 
-	useLayoutEffect(() => {
-		let ctx = gsap.context(() => {
-			tl.current = gsap
-				.timeline()
-				.fromTo(
-					".heyo",
-					{ opacity: 0, ease: 0.2, duration: 1 },
-					{ opacity: 1, ease: 0.2, duration: 1 }
-				)
-				.fromTo(
-					".box",
-					{ opacity: 0, ease: 0.2, duration: 1 },
-					{ opacity: 1, ease: 0.2, duration: 1 }
-				);
-		}, comp);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      tl.current = gsap
+        .timeline()
+        .fromTo(
+          ".heyo",
+          { opacity: 0, ease: 0.2, duration: 1 },
+          { opacity: 1, ease: 0.2, duration: 1 }
+        )
+        .fromTo(
+          ".box",
+          { opacity: 0, ease: 0.2, duration: 1 },
+          { opacity: 1, ease: 0.2, duration: 1 }
+        );
+    }, comp);
 
-		return () => {
-			ctx.revert();
-		};
-	}, []);
+    return () => {
+      ctx.revert();
+    };
+  }, []);
 
-	return (
-		<div className="w-screen h-screen flex flex-col justify-center md:h-screen">
-			<div className="lg:h-full">
-				<div className="container mx-auto flex flex-col lg:justify-center lg:flex-row p-8 lg:p-8 xl:p-0 lg:h-full">
-					<div
-						ref={comp}
-						className=" flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl"
-					>
-						<div className="box">
-							<div className="font-bold pb-4 bg-clip-text text-transparent bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-amber-300 via-red-400 to-slate-700">
-								<span className="text-4xl md:text-5xl xl:text-6xl">
-									Welcome, i'm Sky,
-								</span>
-								<br />{" "}
-								<span className="text-4xl md:text-5xl xl:text-6xl lg:text-7xl font-sans font-bold">
-									Software Engineer and Designer.{" "}
-								</span>
-							</div>
-							<div className="text-white transition-all ease-in-out text-sm md:text-base font-sans font-semibold flex space-x-4 pt-2">
-								<a
-									href="/portfolio"
-									className="hover:bg-red-400 transition-colors duration-300 ease-in-out bg-none border-2 border-red-400 rounded-full py-2 px-4 flex place-items-center"
-								>
-									<div>My Projects</div>
-								</a>
-								<a
-									href="https://twitter.com/Sky_webdev"
-									target="blank_"
-									className="hover:bg-red-400 transition-colors duration-300 ease-in-out bg-none border-2 border-red-400 rounded-full py-2 px-4 flex place-items-center"
-								>
-									<div>Contact Me</div>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center md:h-screen">
+      <div className="lg:h-full">
+        <div className="container mx-auto flex flex-col lg:justify-center lg:flex-row p-8 lg:p-8 xl:p-0 lg:h-full">
+          <div
+            ref={comp}
+            className=" flex flex-col justify-center space-y-4 container mx-auto max-w-sm md:max-w-lg lg:max-w-6xl"
+          >
+            <div className="box">
+              <div className="font-bold pb-4 bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-green-500">
+                <span className="text-4xl md:text-5xl xl:text-6xl">
+                  Welcome, i'm Sky,
+                </span>
+                <br />{" "}
+                <span className="text-4xl md:text-5xl xl:text-6xl lg:text-7xl font-sans font-bold">
+                  Software Engineer and Designer.{" "}
+                </span>
+              </div>
+              <div className="text-white transition-all ease-in-out text-sm md:text-base font-sans font-semibold flex space-x-4 pt-2">
+                <a
+                  href="/portfolio"
+                  className="hover:bg-gradient-to-r from-sky-400 to-green-500 hover:border-transparent transition-colors duration-300 ease-in-out bg-none border-2 border-cyan-400 rounded-md py-2 px-4 flex place-items-center"
+                >
+                  <div>My Projects</div>
+                </a>
+                <a
+                  href="https://twitter.com/Sky_webdev"
+                  target="blank_"
+                  className="bg-gradient-to-r from-sky-400 to-green-500 transition-colors duration-300 ease-in-out border-none rounded-md py-2 px-4 flex place-items-center"
+                >
+                  <div>Contact Me</div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -3,41 +3,40 @@ import { allProjects } from "./allProjects";
 import { BsArrowLeftCircle } from "react-icons/bs";
 
 function Portfolio() {
-	return (
-		<>
-			<div className="h-screen text-white flex flex-col justify-center">
-				<div className="h-full py-24 md:pt-0 md:h-1/2 container mx-auto w-80 md:w-full md:max-w-2xl lg:max-w-3xl space-y-8 md:space-y-10">
-					<div className="text-5xl text-red-400 transition-all hover:scale-105 hover:text-red-300">
-						<a href="/">
-							<BsArrowLeftCircle />
-						</a>
-					</div>
+  return (
+    <>
+      <div className="h-screen text-white flex flex-col justify-center">
+        <div className="h-full py-24 md:pt-0 md:h-1/2 container mx-auto w-80 md:w-full md:max-w-2xl lg:max-w-3xl space-y-8 md:space-y-10">
+          <div className="text-5xl text-cyan-400 transition-all hover:scale-105 hover:text-cyan-700">
+            <a href="/">
+              <BsArrowLeftCircle />
+            </a>
+          </div>
 
-					{allProjects.map((project) => {
-						return (
-							<ul className="">
-								<li
-									key={project.id}
-									className={` transition-transform w-80 z-0 md:w-full md:max-w-3xl hover:scale-105 hover:underline hover:underline-offset-8 hover:decoration-red-400 hover:decoration-4`}
-								>
-									<a
-										href={project.link}
-										target="blank_"
-										className="w-full flex justify-between items-center"
-									>
-										<div className="font-sans text-2xl md:text-4xl text-neutral-300">
-											{project.name}{" "}
-											<span className="text-slate-500"></span>
-										</div>
-									</a>
-								</li>
-							</ul>
-						);
-					})}
-				</div>
-			</div>
-		</>
-	);
+          {allProjects.map((project) => {
+            return (
+              <ul className="">
+                <li
+                  key={project.id}
+                  className={` transition-transform w-80 z-0 md:w-full md:max-w-3xl hover:scale-105 hover:underline hover:underline-offset-8 hover:decoration-cyan-400 hover:decoration-4`}
+                >
+                  <a
+                    href={project.link}
+                    target="blank_"
+                    className="w-full flex justify-between items-center"
+                  >
+                    <div className="font-sans text-2xl md:text-4xl text-neutral-300">
+                      {project.name} <span className="text-slate-500"></span>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Portfolio;
