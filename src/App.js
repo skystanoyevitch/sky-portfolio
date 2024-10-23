33fc5954beced3bac9781a3af8f8c7683d386069
Sky <skystanoyevitch@proton.me>
2024-10-23 08:40:11 -0700

    removed background color and changed to slate-900

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import "./App.css";
 import Homepage from "./Pages/Home/Homepage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// Page Component Imports //
-import Footer from "./Layout/Navigation/Footer";
-import Portfolio from "./Pages/Portfolio/Portfolio";
-
-
 const App = () => (
   <BrowserRouter>
-    <div className="bg-[#FDF8EC]">
+    <div className="bg-slate-900">
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
-      <Footer />
     </div>
   </BrowserRouter>
 );
