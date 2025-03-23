1e4b8af8f7948b97a0fa318020b6d09b23a4b345
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-03-22 22:25:09 -0700

    refactor App component to update background color and font style for improved UI consistency

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import "./App.css";
 import Homepage from "./Pages/Home/Homepage";
 import StrongHabit from "./Pages/StrongHabit/StrongHabit";
@@ -5,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => (
   <BrowserRouter>
-    <div className="min-h-screen bg-terminal-black font-vt">
+    <div className="min-h-screen bg-primary font-sans">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/strong-habit/*" element={<StrongHabit />} />
