159637c56f47f3b05c18c47005e673ae61bb712a
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-03-22 22:25:13 -0700

    refactor Footer component to update styling and improve layout consistency

--- src/Layout/Navigation/Footer.jsx
+++ src/Layout/Navigation/Footer.jsx
@@ -1,18 +1,19 @@
+// Footer.jsx
 import React from "react";
 import Emoji from "./Assests/heart-emoji";
 
 const Footer = () => {
   return (
     <>
-      <footer className="p-4 border-t border-terminal-green bg-terminal-black text-terminal-green font-vt">
+      <footer className="p-4 border-t border-border bg-primary-dark text-text-secondary">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-center">
-            <p className="text-terminal-green-dark text-xs">
-              [SYSTEM STATUS: ONLINE] | [LAST UPDATE: 2025.03.20]
-            </p>
+            <p className="text-text-muted text-xs">[LAST UPDATE: 2025.03.22]</p>
             <p className="text-center text-xs lg:text-sm">
-              Made With <Emoji symbol="❤" label="heart" /> by Sky - {""}
-              <span className="text-terminal-green font-bold">2025</span>
+              Made With{" "}
+              <Emoji symbol="❤" label="heart" className="text-accent" /> by Sky
+              - {""}
+              <span className="text-secondary font-bold">2025</span>
             </p>
           </div>
         </div>
