1c4eccf3e8fed654d8926cc5964d66ce8bcfef57
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-03-22 22:25:17 -0700

    refactor Homepage component to apply autumn gradient background for enhanced visual appeal

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -1,9 +1,10 @@
+// Homepage.jsx
 import Hero from "./Components/Hero";
 import Footer from "../../Layout/Navigation/Footer";
 
 const Homepage = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gradient-autumn">
       <div className="flex-grow">
         <Hero />
       </div>
